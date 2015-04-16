143cccf4d148ec4ae7f2f1edee2d835c5514d797
Muhammad Abdul Jabbaar <raabbajam@gmail.com>
2015-04-16 09:17:21 +0700

    Added google5

---
+++ locals/google5.js
@@ -0,0 +1,14 @@
+var path = require('path');
+module.exports = {
+  projectId: 'linen-adapter-91702',
+  clientId: '528794696886-mg6q9m23qk0fudt7lrv0ptqmqen8le1e.apps.googleusercontent.com',
+  clientSecret: 'qNcAuAMRAskWW9sh0peyqsDK',
+  jwt: {
+    "private_key_id": "3d7e0c03d0615362e1b2e6ef8c18f04f588843eb",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJBT8AIyM78AY00F\nOZWFAhuANcfAYbkYk1mC2igGsSgJR6DuCOIwKOtnxfDum9P7R85gKg9HgCzQATM1\nfy9fILuVr9s5sRyRd2cplzwFoNXkP9GSyi2VFP2HIL0MjsVZKgMfcO7Q9LcPT1W7\nU96m4Nez4izeM/H/nvxk40sudfnDAgMBAAECgYAe4KZXgfAKLAYSo0yHjZY+1fYr\njyAZHoXfv5v2tj/OhfW6G1LxOUZ33pK9kf7qojbhkzdMX4qvi6zGDOrx8pMo3IT9\n5/rjK/vo5S+DiVoCg7UALhxlGEm8LTJg7Fv27ObJnp4tVkeKMgIaMC009aDlkfS0\nTPOdp85buwQUP2tlmQJBAMA5tzV+ymGoIaA0RkQy0c39Xxsg6zAID+zEj2ybzns9\ncvp6tOJnRUUMaf5HPa66TpBOjQEeL8d3QdTmXGqkMjcCQQDANiMx/gIW/r7X3yPS\n5JsyslYS3V1nksDRCJuQHwQ3VyCdeHIMswLvC+epahc2mLznbcCECmi+8BqmMZEN\n2F7VAkEAsGhHKmDheh3CPjO7CHta75/G2C6tp7a+Zps8xH94TsFl81kJB32epG9w\nFltq0B9KWkS8QnphfryDPc7qoTNuoQJAXutdPhbeGVT8EcbT/hV+vu5qqy4+LiO+\nrHu/ttBaWKniJWGRkMvmKv6KDJOjmwaaYv9EDvDFAWW5A1F+/INZcQJAGhkk0Urt\nPeyp0/Qqey2mM+Y31vtRXfiPy4EPmm6Lyxnvyp6rVXVdvQB9L+3x681UX+71W61x\nst2dBKdeHDt3Eg\u003d\u003d\n-----END PRIVATE KEY-----\n",
+    "client_email": "528794696886-uqrho8q6esoviog00c1dr76eda6u327e@developer.gserviceaccount.com",
+    "client_id": "528794696886-uqrho8q6esoviog00c1dr76eda6u327e.apps.googleusercontent.com",
+    "type": "service_account",
+    "key_file": path.join(__dirname, "googleapi-privatekey5.pem")
+  }
+};
